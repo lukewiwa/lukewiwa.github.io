2f2169d071ea1e32f66edd6fc3cb3eb51b292d31
lukewiwa <luke.wiwa@gmail.com>
2018-11-07 17:33:06 +1100

    added styled nav component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Link } from 'react-static'
+import { Router } from 'react-static'
 import { hot } from 'react-hot-loader'
 //
 import Routes from 'react-static-routes'
@@ -10,12 +10,12 @@ import './app.css'
 const App = () => (
   <Router>
     <div>
-      <Nav 
-        title={"Wiwa"}
+      <Nav
+        title="Wiwa"
         headings={[
-          {link: "blog", title: "Blog"},
-          {link: "gymnastics", title: "Gymnastics"},
-          {link: "about", title: "About"},
+          { link: 'blog', title: 'Blog' },
+          { link: 'gymnastics', title: 'Gymnastics' },
+          { link: 'about', title: 'About' },
         ]}
       />
       <div className="flex-center">
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,37 +2,67 @@ import React from 'react'
 import { Link } from 'react-static'
 import styled from 'styled-components'
 
-const StyledNav = styled.nav`
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: space-between;
+const Container = styled.nav`
+  border-bottom: 1px solid #f2f2f2;
 
-  @media only screen and (max-width : 360px) {
-    display: flex;
-    flex-flow: column;
-    justify-content: center;
+  background-color: #f2f2f2;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 3em 3em;
+  grid-template-areas:
+    "brand"
+    "menu";
+  justify-items: center;
+  align-items: center;
+
+  @media only screen and (min-width : 42em) {
+    grid-template-columns: minmax(1em, 1fr) 20em minmax(auto, 20em) 20em minmax(1em, 1fr) ;
+    grid-template-rows: 4em;
+    grid-template-areas: ". brand . menu .";
+  }
+
+  a {
+    color: #777;
+  }
+`
+
+const Brand = styled.div`
+  grid-area: brand;
+  font-weight: 800;
+  @media only screen and (min-width : 42em) {
+    justify-self: start;
+  }
+`
+
+const Menu = styled.div`
+  grid-area: menu;
+  @media only screen and (min-width : 42em) {
+    justify-self: end;
+  }
+`
+
+const Item = styled(Link)`
+  margin: 1em;
+  &.active {
+    font-weight: 600;
   }
 `
 
 export default function Nav (props) {
   return (
-    <StyledNav>
-      <div>
+    <Container>
+      <Brand>
         <Link exact to="/">
           {props.title}
         </Link>
-      </div>
-      <div>
-        {props.headings.map((heading, i) => {
-          return (
-            <span key={i}> 
-              <Link to={`/${heading.link}`}>
-                {heading.title}
-              </Link>
-            </span>
-          )
-        })}
-      </div>
-    </StyledNav>
+      </Brand>
+      <Menu>
+        {props.headings.map((heading, i) => (
+          <Item key={i} to={`/${heading.link}`}>
+            {heading.title}
+          </Item>
+        ))}
+      </Menu>
+    </Container>
   )
 }
