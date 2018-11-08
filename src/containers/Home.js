f542ed1e3eb290b54b8b12236c8d3b4f2a394aad
lukewiwa <luke.wiwa@gmail.com>
2018-11-09 08:46:42 +1100

    inline styles

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,7 +4,7 @@ import { withSiteData } from 'react-static'
 
 export default withSiteData(() => (
   <div>
-    <h1 style={{ textAlign: 'center' }}>Luke Wiwatowski</h1>
-    <h2 style={{ textAlign: 'center' }}>Gymnastics Engineering Miscellaneous</h2>
+    <h1 style={{ fontSize: '4rem', fontWeight: 20, textAlign: 'center' }}>Luke Wiwatowski</h1>
+    <h2 style={{ fontSize: '2rem', fontWeight: 20, textAlign: 'center' }}>Gymnastics Engineering Miscellaneous</h2>
   </div>
 ))
