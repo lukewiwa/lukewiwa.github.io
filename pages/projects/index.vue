69f5a779270b10f6725186b43aa1c046583b1e8c
lukewiwa <luke.wiwa@gmail.com>
2020-12-22 16:10:21 +1100

    Add various project details

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="max-w-screen-md mx-auto p-4 space-y-10">
+  <div class="max-w-screen-md mx-auto p-4 space-y-8">
+    <section class="flex flex-col space-y-2">
+      <a href="https://lukewiwa.com/judge-of-character/" class="bloglist-title">
+        Judge of Character
+      </a>
+      <p>
+        A web game using client side tesseract and notepad libraries. It's
+        frustratingly fun.
+      </p>
+    </section>
+    <section class="flex flex-col space-y-2">
+      <a
+        href="https://lukewiwa.com/pwa-share-target-data-revealer/"
+        class="bloglist-title"
+      >
+        PWA Share Target Data Revealer
+      </a>
+      <p>
+        If you have a PWA installed on android you can register it as a
+        <a href="https://web.dev/web-share-target/"> share target</a>. For
+        debugging purposes this PWA allows you to see exactly what type of data
+        will be transferred through the share intent. Add this site to your
+        homescreen from android chrome and then from any android app share to
+        this PWA to reveal the data being transferred.
+      </p>
+    </section>
+    <section class="flex flex-col space-y-2">
+      <a href="https://lukewiwa.com/shout/" class="bloglist-title">
+        Shout!
+      </a>
+      <p>
+        A proof of concept for the most basic Progressive Web App using React.
+        Make text BIGGER.
+      </p>
+    </section>
     <section class="flex flex-col space-y-2">
       <nuxt-link to="/projects/code-of-points" class="bloglist-title">
         Code of Points
